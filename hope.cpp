85555895f6134d3c5f4b4e10b64736b44171e9db
sayidazim2008 <114296350+sayidazim2008@users.noreply.github.com>
2023-12-31 12:29:07 +0300

    Add files via upload
    добавил файл

---
+++ hope.cpp
@@ -0,0 +1,30 @@
+#define _GLIBCXX_DEBUG
+#define _GLIBCXX_DEBUG_PEDANTIC
+
+// ВОТ БЫ НЕ ЗАБЫТЬ ЭТО ЗАКОМЕНТИТЬ
+
+
+#include "bits/stdc++.h"
+typedef long long ll;
+typedef unsigned long long ull;
+#define int ll
+#define INF ((ll)1e9)
+#define F first
+#define S second
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fr(i, a, b) for(auto i=(a); i<(b); i++)
+#define sz(x) (int)x.size()
+#define print(a) for(int i_=0; i_<sz(a); i_++)cout << a[i_] << ' '
+#define input(a) for(int i_=0; i_<sz(a); i_++)cin >> a[i_]
+#define nl '\n'
+
+//ДЕВОЧКА ЗАЙКА ВАНЛАВ
+//СИСЬКИ БРБРБР
+
+using namespace std;
+
+signed main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+}
